03dfa8b69543abcad5e6d71205814ed082a78a76
danielpellegrini <danielpellegrini87@gmail.com>
2021-06-07 15:39:58 +0200

    secure_asset updated

--- resources/views/layout/main-layout.blade.php
+++ resources/views/layout/main-layout.blade.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
   <title>@yield('title_name')</title>
 </head>
 <body>
@@ -19,7 +19,7 @@
   @include('components/footer')
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="{{asset('js/app.js')}}" charset="utf-8"></script>
+  <script src="{{secure_asset('js/app.js')}}" charset="utf-8"></script>
   
 </body>
 </html>
